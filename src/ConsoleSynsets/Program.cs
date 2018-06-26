f025c87eccbe2612d5d917eb3a2aa6342486de5e
Jonathan Fonseca V <jfonseca1510@gmail.com>
2018-06-25 22:31:21 -0600

    Adding None and inverse relations
    - Adding None and inverse relations.
    - Refactoring a little bit the code.
    - Changing the way we get the resources path.
    - Ignoring two-words words.

--- src/ConsoleSynsets/Program.cs
+++ src/ConsoleSynsets/Program.cs
@@ -14,53 +14,79 @@ namespace ConsoleSynsets
 {
     class Program
     {
-        private static WordNetEngine _wordNetEngine = new WordNetEngine(@"C:\Users\Christopher\Documents\GitHub\WordNet\resources", false);
+        private static String currentPath = Directory.GetCurrentDirectory();
+        private static WordNetEngine _wordNetEngine = new WordNetEngine(Path.Combine(currentPath, @"..\..\..\..\resources"), false);
+        static Random rnd = new Random();
+
         static void Main(string[] args)
         {
             List<String> relaciones = new List<String>();
 
             if (File.Exists("words.txt"))
             {
-                // Usernames are listed first in users.ul, and are followed by a period and then the password associated with that username.
-                StreamReader reader = new StreamReader("words.txt");
-                string line;
+                var file = File.CreateText("relaciones.csv");
+                List<String> allLinesText = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "words.txt")).ToList();
+                Set<SynSet> synSetsToShow = null;              
 
-                Set<SynSet> synSetsToShow = null;
-                List<String> rels = new List<string>();
-
-                using (var file = File.CreateText("relaciones.csv"))
+                foreach (String line in allLinesText)
                 {
-                    while ((line = reader.ReadLine()) != null)
+                    try
                     {
-                        try
-                        {
-                            synSetsToShow = _wordNetEngine.GetSynSets(line, LAIR.ResourceAPIs.WordNet.WordNetEngine.POS.Noun);
-                            //main SynSet
-                            SynSet s1 = synSetsToShow.Last();
+                        synSetsToShow = _wordNetEngine.GetSynSets(line, LAIR.ResourceAPIs.WordNet.WordNetEngine.POS.Noun);
+                        //main SynSet
+                        SynSet s1 = synSetsToShow.Last();
 
-                            foreach (WordNetEngine.SynSetRelation synSetRelation in s1.SemanticRelations)
+                        foreach (WordNetEngine.SynSetRelation synSetRelation in s1.SemanticRelations)
+                        {
+                            if (synSetRelation.ToString().Equals("Hypernym") || synSetRelation.ToString().Equals("Hyponym"))
                             {
-                                if (synSetRelation.ToString().Equals("Hypernym") || synSetRelation.ToString().Equals("Hyponym"))
+                                String oppositeSynSetRelation = synSetRelation.ToString() == "Hypernym" ? "Hyponym" : "Hypernym";
+                                List<String> synSetsAdded = new List<String>();
+
+                                SynSet s2 = s1.GetRelatedSynSets(synSetRelation, false).First();
+
+                                if (!s1.Words[0].Contains("_") && !s2.Words[0].Contains("_"))
                                 {
-                                    //related SynSet
-                                    SynSet s2 = s1.GetRelatedSynSets(synSetRelation, false).First();
-                                    //fill the line
                                     String csv_line = s1.Words[0] + "," + s2.Words[0] + "," + synSetRelation;
+                                    String csv_line2 = s2.Words[0] + "," + s1.Words[0] + "," + oppositeSynSetRelation;
 
-                                    file.WriteLine(csv_line);
-                                    //out of the foreach loop
-                                    //break;
-                                }
+                                    if (!synSetsAdded.Contains(csv_line) && !synSetsAdded.Contains(csv_line2))
+                                    {
+                                        synSetsAdded.Add(csv_line);
+                                        synSetsAdded.Add(csv_line2);
+                                        file.WriteLine(csv_line);
+                                        file.WriteLine(csv_line2);
+                                    }
+
+                                    int randomPosition = rnd.Next(allLinesText.Count());
+                                    String randomWord = allLinesText[randomPosition];                                    
+
+                                    while (randomWord != "")
+                                    {
+                                        if (!s1.Words.Contains(randomWord) && !s2.Words.Contains(randomWord))
+                                        {
+                                            String csv_line3 = s1.Words[0] + "," + randomWord + ",None";
+                                            file.WriteLine(csv_line3);
+
+                                            randomWord = "";
+                                        }                   
+                                        else
+                                        {
+                                            randomPosition = rnd.Next(allLinesText.Count());
+                                            randomWord = allLinesText[randomPosition];
+                                        }
+                                    }
+                                }                                
                             }
-
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
                         }
                     }
-                    reader.Close();
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
                 }
+
+                file.Close();
             }
         }
     }
